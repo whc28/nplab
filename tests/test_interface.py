05de58533c623a1fa1f103a771466ecdbbf35cfc
Richard Bowman <richard.bowman@cantab.net>
2015-10-04 21:04:52 +0100

    Show_gui now only makes one UI
    show_gui will now only call get_qt_ui once in the life of an object;
    thereafter it will return the existing QWidget and show it gain.  TODO:
    same behaviour for Traits,  Could be useful if it brought the window to
    the front as well as showing it.

---
+++ tests/test_interface.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 03 21:05:56 2015
+
+@author: rwb27
+"""
+
+from nplab.instrument import Instrument
+from nplab.utils.gui import qt, qtgui
+import traits, traitsui
+from traits.api import HasTraits, Property, Instance, Float, String, Button
+import traitsui
+from traitsui.api import View, Item, HGroup, VGroup
+
+class InstrumentA(Instrument):
+    def get_qt_ui(self):
+        l = qtgui.QLabel()
+        l.setText("InstrumentA Interface")
+        return l
+        
+class InstrumentB(Instrument, HasTraits):
+    description = String("Description...")
+    traits_view = View(Item(name="description"))
+    
+if __name__ == "__main__":
+    a = InstrumentA()
+    b = InstrumentB()
